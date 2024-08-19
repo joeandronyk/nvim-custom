7fb11a055bc0d078f3e246bd7e5ff563b7b675b8
Joe Andronyk <joeandronyk@gmail.com>
2024-08-18 23:41:17 -0700

    Updated colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -184,6 +184,7 @@
 --     end,
 --   },
 -- }
+
 return {
   {
     'rebelot/kanagawa.nvim',
@@ -212,28 +213,52 @@ return {
               ui = {
                 bg_gutter = 'none',
               },
-              syn = {
-                comment = 'yellow',
-              },
             },
           },
         },
         overrides = function(colors)
           local theme = colors.theme
           return {
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = 'none' },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = 'none' },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = 'none' },
+            TelescopePreviewNormal = { bg = 'none' },
+            TelescopePreviewBorder = { fg = theme.ui.bg_dim, bg = 'none' },
             NormalFloat = { bg = 'none' },
-            FloatBorder = { bg = 'none' },
             FloatTitle = { bg = 'none' },
-
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
+            FloatBorder = { bg = 'none' },
             LazyNormal = { bg = 'none', fg = theme.ui.fg_dim },
             MasonNormal = { bg = 'none', fg = theme.ui.fg_dim },
+            Pmenu = { fg = theme.ui.shade0, bg = 'none', blend = vim.o.pumblend },
+            PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            -- TroubleNormal = { link = 'NormalDark' },
+            -- TroubleNormalNC = { link = 'TroubleNormal' },
+            -- NeoTreeNormal = { link = 'NormalDark' },
+            -- DiagnosticVirtualTextError = {
+            --   fg = theme.diag.error,
+            --   bg = c(theme.diag.error):blend(theme.ui.bg, 0.95):to_hex(),
+            -- },
+            -- DiagnosticVirtualTextWarn = {
+            --   fg = theme.diag.warning,
+            --   bg = c(theme.diag.warning):blend(theme.ui.bg, 0.95):to_hex(),
+            -- },
+            -- DiagnosticVirtualTextHint = {
+            --   fg = theme.diag.hint,
+            --   bg = c(theme.diag.hint):blend(theme.ui.bg, 0.95):to_hex(),
+            -- },
+            -- DiagnosticVirtualTextInfo = {
+            --   fg = theme.diag.info,
+            --   bg = c(theme.diag.info):blend(theme.ui.bg, 0.95):to_hex(),
+            -- },
+            -- DiagnosticVirtualTextOk = {
+            --   fg = theme.diag.ok,
+            --   bg = c(theme.diag.ok):blend(theme.ui.bg, 0.95):to_hex(),
+            -- },
           }
         end,
         theme = 'wave',    -- Load "wave" theme when 'background' option is not set
