89c7a9e1584e676489414e535892a30902a15276
Andronyk, Joe <jandronyk@ea.com>
2024-11-13 10:47:07 -0800

    Showing hidden files in grug--far

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -4,10 +4,25 @@ return {
   vim.keymap.set('n', '<leader>fr', ':GrugFar<CR>', { desc = 'Find and Replace' }),
 
   config = function()
-    require('grug-far').setup {
-      -- options, see Configuration section below
-      -- there are no required options atm
-      -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+    require 'grug-far'.setup {
+      engines = {
+        ripgrep = {
+          extraArgs =
+              '--no-heading -.ni --path-separator=/'
+              .. ' -g !.git'
+              .. ' -g !.github'
+              .. ' -g !*cache'
+              .. ' -g !bin'
+              .. ' -g !out'
+              .. ' -g !build'
+              .. ' -g !target'
+              .. ' -g !vendor'
+              .. ' -g !dist'
+              .. ' -g !node_modules'
+        },
+      },
+      windowCreationCommand = 'vert topleft split',
+      wrap = false,
     }
   end,
 }
