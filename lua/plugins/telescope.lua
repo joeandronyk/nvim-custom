a5d5c75a42615bf1167f73efaac160e32b1b4826
Andronyk, Joe <jandronyk@ea.com>
2024-11-20 06:40:28 -0800

    Fixing telescop searching

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -66,44 +66,44 @@ return {
           --   '.pyc',
           -- },
           vimgrep_arguments = {
-                "rg",
-                "--follow",        -- Follow symbolic links
-                "--hidden",        -- Search for hidden files
-                "--no-heading",    -- Don't group matches by each file
-                "--with-filename", -- Print the file path with the matched lines
-                "--line-number",   -- Show line numbers
-                "--column",        -- Show column numbers
-                "--smart-case",    -- Smart case search
+            'rg',
+            '--follow', -- Follow symbolic links
+            '--hidden', -- Search for hidden files
+            '--no-heading', -- Don't group matches by each file
+            '--with-filename', -- Print the file path with the matched lines
+            '--line-number', -- Show line numbers
+            '--column', -- Show column numbers
+            '--smart-case', -- Smart case search
 
-                -- Exclude some patterns from search
-                "--glob=!**/.git/*",
-                "--glob=!**/.idea/*",
-                "--glob=!**/.vscode/*",
-                "--glob=!**/build/*",
-                "--glob=!**/dist/*",
-                "--glob=!**/yarn.lock",
-                "--glob=!**/package-lock.json",
-              },
+            -- Exclude some patterns from search
+            '--glob=!**/.git/*',
+            '--glob=!**/.idea/*',
+            '--glob=!**/.vscode/*',
+            '--glob=!**/build/*',
+            '--glob=!**/dist/*',
+            '--glob=!**/yarn.lock',
+            '--glob=!**/package-lock.json',
+          },
         },
         pickers = {
-            find_files = {
-              hidden = true,
-              -- needed to exclude some files & dirs from general search
-              -- when not included or specified in .gitignore
-              find_command = {
-                "rg",
-                "--files",
-                "--hidden",
-                "--glob=!**/.git/*",
-                "--glob=!**/.idea/*",
-                "--glob=!**/.vscode/*",
-                "--glob=!**/build/*",
-                "--glob=!**/dist/*",
-                "--glob=!**/yarn.lock",
-                "--glob=!**/package-lock.json",
-              },
+          find_files = {
+            hidden = true,
+            -- needed to exclude some files & dirs from general search
+            -- when not included or specified in .gitignore
+            find_command = {
+              'rg',
+              '--files',
+              '--hidden',
+              '--glob=!**/.git/*',
+              '--glob=!**/.idea/*',
+              '--glob=!**/.vscode/*',
+              '--glob=!**/build/*',
+              '--glob=!**/dist/*',
+              '--glob=!**/yarn.lock',
+              '--glob=!**/package-lock.json',
             },
           },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -131,7 +131,7 @@ return {
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Find Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers' })
       -- vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find Files' })
-      vim.keymap.set('n', '<leader><leader>', ':Telescope find_files hidden=true<CR>' , { desc = 'Find Files' })
+      vim.keymap.set('n', '<leader><leader>', ':Telescope find_files hidden=true<CR>', { desc = 'Find Files' })
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
