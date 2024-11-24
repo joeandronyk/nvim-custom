83ba531f6214e111cff5a6a82eb134a531331d01
Joe Andronyk <joeandronyk@gmail.com>
2024-11-23 21:46:07 -0800

    Add lualine and fix colorscheme a bit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,7 +17,7 @@
 --     end,
 --   },
 -- }
-
+--
 -- return {
 --   {
 --     'catppuccin/nvim',
@@ -26,7 +26,7 @@
 --     opts = {
 --       transparent_background = true, -- disables setting the background color.
 --       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
---       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--       term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
 --       no_italic = false, -- Force no italic
 --       no_bold = false, -- Force no bold
 --       no_underline = false, -- Force no underline
@@ -83,16 +83,6 @@
 --       -- vim.cmd.colorscheme 'catppuccin-macchiato'
 --       vim.cmd.colorscheme 'catppuccin-mocha'
 --     end,
---   },
--- }
-
--- return {
---   { "gbprod/nord.nvim", name = "nord", priority = 1000 },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "nord",
---     },
 --   },
 -- }
 
@@ -191,15 +181,16 @@ return {
     priority = 1000,
     config = function()
       require('kanagawa').setup {
-        compile = false,  -- enable compiling the colorscheme
+        compile = false, -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
         functionStyle = { bold = true },
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = true,    -- do not set background color
-        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        transparent = true, -- do not set background color
+        globalStatus = true,
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
         colors = {
           theme = {
@@ -236,6 +227,7 @@ return {
             PmenuSbar = { bg = theme.ui.bg_m1 },
             PmenuThumb = { bg = theme.ui.bg_p2 },
             NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            CursorLine = { bg = '#282c41', fg = '#acb4c2' }, -- match lualine's nightfly theme
             -- TroubleNormal = { link = 'NormalDark' },
             -- TroubleNormalNC = { link = 'TroubleNormal' },
             -- NeoTreeNormal = { link = 'NormalDark' },
@@ -261,8 +253,8 @@ return {
             -- },
           }
         end,
-        theme = 'wave',    -- Load "wave" theme when 'background' option is not set
-        background = {     -- map the value of 'background' option to a theme
+        theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
           dark = 'dragon', -- try "dragon" !
           light = 'lotus',
         },
