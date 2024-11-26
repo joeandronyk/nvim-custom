adf9eeb456f58ee7d8389ca9916ef45449aa50f5
Joe Andronyk <joeandronyk@gmail.com>
2024-11-25 16:42:12 -0800

    Adding in more to the config and setting the width to be wider.

--- lua/plugins/buffer-manager.lua
+++ lua/plugins/buffer-manager.lua
@@ -1,15 +1,35 @@
 return {
-  {
-    'j-morano/buffer_manager.nvim',
-    dependencies = 'nvim-lua/plenary.nvim',
-    keys = {
-      {
-        '<leader>bb',
-        function()
-          require('buffer_manager.ui').toggle_quick_menu()
-        end,
-        desc = 'Open Buffer Manager',
-      },
+  'j-morano/buffer_manager.nvim',
+  dependencies = 'nvim-lua/plenary.nvim',
+  keys = {
+    {
+      '<leader>bb',
+      function()
+        require('buffer_manager.ui').toggle_quick_menu()
+      end,
+      desc = 'Open Buffer Manager',
     },
   },
+  config = function()
+    require('buffer_manager').setup {
+      line_keys = '1234567890',
+      select_menu_item_commands = {
+        edit = {
+          key = '<CR>',
+          command = 'edit',
+        },
+      },
+      focus_alternate_buffer = false,
+      short_file_names = false,
+      short_term_names = false,
+      loop_nav = true,
+      width = 0.55,
+      highlight = '',
+      win_extra_options = {},
+      borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+      format_function = nil,
+      order_buffers = nil,
+      show_indicators = nil,
+    }
+  end,
 }
