fabbafc4e915de7c32cd82d85aa6855ccaf60479
Joe Andronyk <joeandronyk@gmail.com>
2024-12-08 12:45:21 -0800

    Showing partial path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -25,7 +25,7 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
@@ -33,7 +33,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {},
