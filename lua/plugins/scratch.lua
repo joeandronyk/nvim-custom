e40a95740fb2dc5bf33b2e73963aba060c6cf751
Joe Andronyk <joeandronyk@gmail.com>
2025-02-06 08:17:27 -0800

    Removing commented out telescope

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -3,7 +3,6 @@ return {
   event = 'VeryLazy',
   dependencies = {
     { 'ibhagwan/fzf-lua' }, --optional: if you want to use fzf-lua to pick scratch file. Recommanded, since it will order the files by modification datetime desc. (require rg)
-    -- { 'nvim-telescope/telescope.nvim' }, -- optional: if you want to pick scratch file by telescope
     { 'stevearc/dressing.nvim' }, -- optional: to have the same UI shown in the GIF
   },
   vim.keymap.set('n', '<leader>sn', '<cmd>Scratch<cr>', { desc = 'New Scratch File' }),
