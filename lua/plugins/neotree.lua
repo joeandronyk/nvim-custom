6a305be0da8114f7cabaadd927fe8a65eb6a20e4
Joe Andronyk <joeandronyk@gmail.com>
2025-02-18 19:38:10 -0800

    Update

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -108,46 +108,46 @@ return {
         -- see `:h neo-tree-custom-commands-global`
         commands = {
           copy_selector = function(state)
-          local node = state.tree:get_node()
-          local filepath = node:get_id()
-          local filename = node.name
-          local modify = vim.fn.fnamemodify
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
 
-          local vals = {
-            ["BASENAME"] = modify(filename, ":r"),
-            ["EXTENSION"] = modify(filename, ":e"),
-            ["FILENAME"] = filename,
-            ["PATH (CWD)"] = modify(filepath, ":."),
-            ["PATH (HOME)"] = modify(filepath, ":~"),
-            ["PATH"] = filepath,
-            ["URI"] = vim.uri_from_fname(filepath),
-          }
+            local vals = {
+              ["BASENAME"] = modify(filename, ":r"),
+              ["EXTENSION"] = modify(filename, ":e"),
+              ["FILENAME"] = filename,
+              ["PATH (CWD)"] = modify(filepath, ":."),
+              ["PATH (HOME)"] = modify(filepath, ":~"),
+              ["PATH"] = filepath,
+              ["URI"] = vim.uri_from_fname(filepath),
+            }
 
-          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
-          if vim.tbl_isempty(options) then
-            vim.notify("No values to copy", vim.log.levels.WARN)
-            return
-          end
-          table.sort(options)
-          vim.ui.select(options, {
-            prompt = "Choose to copy to clipboard:",
-            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
-          },
-          function(choice)
-            local result = vals[choice]
-            if result then
-              vim.notify(("Copied: `%s`"):format(result))
-              vim.fn.setreg("+", result)
+            local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+            if vim.tbl_isempty(options) then
+              vim.notify("No values to copy", vim.log.levels.WARN)
+              return
             end
-          end)
-        end,
-        find_in_dir = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          require("telescope.builtin").find_files {
-            cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
-          }
-        end,
+            table.sort(options)
+            vim.ui.select(options, {
+              prompt = "Choose to copy to clipboard:",
+              format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+            },
+            function(choice)
+              local result = vals[choice]
+              if result then
+                vim.notify(("Copied: `%s`"):format(result))
+                vim.fn.setreg("+", result)
+              end
+            end)
+          end,
+          find_in_dir = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require("telescope.builtin").find_files {
+              cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
+            }
+          end,
         },
         window = {
           position = 'left',
