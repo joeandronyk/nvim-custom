d1e2187f0e17d8b2defabf097c209b2503496a88
Joe Andronyk <joeandronyk@gmail.com>
2025-02-21 08:45:39 -0800

    Disabling neotree for now in

--- lua/plugins/disabled/neotree.lua
+++ lua/plugins/disabled/neotree.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
+    enabled = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -114,66 +115,68 @@ return {
             local modify = vim.fn.fnamemodify
 
             local vals = {
-              ["BASENAME"] = modify(filename, ":r"),
-              ["EXTENSION"] = modify(filename, ":e"),
-              ["FILENAME"] = filename,
-              ["PATH (CWD)"] = modify(filepath, ":."),
-              ["PATH (HOME)"] = modify(filepath, ":~"),
-              ["PATH"] = filepath,
-              ["URI"] = vim.uri_from_fname(filepath),
+              ['BASENAME'] = modify(filename, ':r'),
+              ['EXTENSION'] = modify(filename, ':e'),
+              ['FILENAME'] = filename,
+              ['PATH (CWD)'] = modify(filepath, ':.'),
+              ['PATH (HOME)'] = modify(filepath, ':~'),
+              ['PATH'] = filepath,
+              ['URI'] = vim.uri_from_fname(filepath),
             }
 
-            local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+            local options = vim.tbl_filter(function(val)
+              return vals[val] ~= ''
+            end, vim.tbl_keys(vals))
             if vim.tbl_isempty(options) then
-              vim.notify("No values to copy", vim.log.levels.WARN)
+              vim.notify('No values to copy', vim.log.levels.WARN)
               return
             end
             table.sort(options)
 
             local lines = {}
             for _, option in ipairs(options) do
-              table.insert(lines, ("%s: %s"):format(option, vals[option]))
+              table.insert(lines, ('%s: %s'):format(option, vals[option]))
             end
 
-            local win = require('snacks.win')({
+            local win = require 'snacks.win' {
               width = 0.5,
               height = 0.3,
               row = 0.3,
               col = 0.3,
-              border = "rounded",
-              title = "Choose to copy to clipboard",
+              border = 'rounded',
+              title = 'Choose to copy to clipboard',
               minimal = true,
               enter = true,
               wo = {
                 wrap = false,
                 cursorline = true,
-                winhighlight = "Normal:SnacksNormal,NormalNC:SnacksNormalNC",
+                winhighlight = 'Normal:SnacksNormal,NormalNC:SnacksNormalNC',
               },
               keys = {
-                q = "close",  -- Close window on pressing 'q'
-                ["<CR>"] = function(self)
-                  local choice = vim.fn.line(".")
+                q = 'close', -- Close window on pressing 'q'
+                ['<CR>'] = function(self)
+                  local choice = vim.fn.line '.'
                   local key = options[choice]
                   local result = vals[key]
                   if result then
-                    vim.notify(("Copied: `%s`"):format(result))
-                    vim.fn.setreg("+", result)
+                    vim.notify(('Copied: `%s`'):format(result))
+                    vim.fn.setreg('+', result)
                   end
                   self:close()
                 end,
               },
               text = lines,
-            })
+            }
 
             win:show()
           end,
-        find_in_dir = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          require("telescope.builtin").find_files {
-            cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
-          }
-        end
+          find_in_dir = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require('telescope.builtin').find_files {
+              cwd = node.type == 'directory' and path or vim.fn.fnamemodify(path, ':h'),
+            }
+          end,
         },
         window = {
           position = 'left',
