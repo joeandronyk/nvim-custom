7ce9d9166c0464261e5e01862ea86ecbc414beeb
Andronyk, Joe <jandronyk@ea.com>
2025-03-02 20:07:43 -0800

    working for maya but not cleaned up
    still have changes in nvim-dap-python

--- lua/plugins/debug-python.lua
+++ lua/plugins/debug-python.lua
@@ -1,28 +1,46 @@
 return {
   'mfussenegger/nvim-dap-python',
   ft = 'python',
-  -- event = 'VeryLazy',
   lazy = true,
+  enabled = true,
   dependencies = { 'mfussenegger/nvim-dap' },
   keys = {
     { '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", mode = 'n', desc = 'Add Breakpoint' },
   },
   config = function()
-    require('dap-python').setup 'python'
+    -- The python path needs to be an absolute path.  Here we hijack venv-selection
+    -- to update the python path in dap-python.
+    local path = require 'venv-selector.path'
+    path.update_python_dap = function()
+      local dap_python_installed, dap_python = pcall(require, 'dap-python')
+      local dap_installed, dap = pcall(require, 'dap')
+      if dap_python_installed and dap_installed then
+        local python_path = require('venv-selector').python()
+        require('dap-python').setup(python_path)
+        print("Setting dap python interpreter to '" .. python_path .. "'")
+      else
+        log.debug 'Debugger not enabled: dap or dap-python not installed.'
+      end
+    end
 
     table.insert(require('dap').configurations.python, {
       type = 'debugpy',
       request = 'attach',
+      pythonPath = python_path,
+      -- request = 'launch',
       name = 'My Python Debugger: Attach using Process Id',
       processId = require('dap.utils').pick_process,
       justMyCode = true,
-      -- host = "127.0.0.1",
+      host = '127.0.0.1',
       port = '5678',
       program = '${file}',
       cwd = vim.fn.getcwd(),
       redirectOutput = false,
       logToFile = true,
-      -- sourceMaps = true,
+      sourceMaps = true,
+      timeout = 50000,
+      initialize_timeout_sec = 100,
+      console = 'integratedTerminal',
     })
 
     table.insert(require('dap').configurations.python, {
--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -71,15 +71,19 @@ return {
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
-          pause = '⏸',
+          
+          pause = '',
           play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          -- step_into = '⏎',
+          step_into = '󰆹',
+          -- step_over = '⏭',
+          step_over = '',
+          -- step_out = '⏮',
+          step_out = '󰆸',
+          step_back = '',
+          run_last = '',
+          terminate = '',
+          disconnect = '',
         },
       },
     }
