0662bfdf312e1f1907b1b7a1ec26313d61f31681
Andronyk, Joe <jandronyk@ea.com>
2025-03-03 08:59:25 -0800

    improvements

--- lua/plugins/debug-python.lua
+++ lua/plugins/debug-python.lua
@@ -16,7 +16,8 @@ return {
       ---@diagnostic disable-next-line: undefined-field
       local host = (config.connect or config).host or '127.0.0.1'
 
-      cb {
+      ---@type dap.ExecutableAdapter
+      local adapter = {
         type = "executable",  -- this normally is 'server', but overrode it to work with maya.
         port = assert(port, "`connect.port` is required for a python `attach` configuration"),
         host = host,
@@ -26,44 +27,46 @@ return {
         command = require('venv-selector').python(),
         args = { "-m", "debugpy.adapter" },
       }
-      else
-        cb {
-          type = 'executable',
-          command = require('venv-selector').python(),
-          args = { '-m', 'debugpy.adapter' },
-          options = {
-            source_filetype = 'python',
-          },
-        }
-      end
+      cb(adapter)
+    else
+      ---@type dap.ExecutableAdapter
+      adapter = {
+        type = 'executable',
+        command = require('venv-selector').python(),
+        args = { '-m', 'debugpy.adapter' },
+        options = {
+          source_filetype = 'python',
+        },
+      }
     end
+  end
 
-    table.insert(require('dap').configurations.python, {
-      type = 'debugpy',
-      request = 'attach',
-      pythonPath = require('venv-selector').python(),
-      name = 'My Python Debugger: Attach using Process Id',
-      processId = require('dap.utils').pick_process,
-      justMyCode = true,
-      host = '127.0.0.1',
-      port = '5678',
-      program = '${file}',
-      cwd = vim.fn.getcwd(),
-      redirectOutput = false,
-      logToFile = true,
-      sourceMaps = true,
-      console = 'nil',
-    })
+  table.insert(require('dap').configurations.python, {
+    type = 'debugpy',
+    request = 'attach',
+    pythonPath = require('venv-selector').python(),
+    name = 'My Python Debugger: Attach using Process Id',
+    processId = require('dap.utils').pick_process,
+    justMyCode = true,
+    host = '127.0.0.1',
+    port = '5678',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    redirectOutput = false,
+    logToFile = true,
+    sourceMaps = true,
+    console = 'integratedTerminal',
+  })
 
-    table.insert(require('dap').configurations.python, {
-      -- The first three options are required by nvim-dap
-      type = 'python',
-      request = 'launch',
-      name = 'Python3: Launch file',
-      program = '${file}',
-      pythonPath = require('venv-selector').venv(),
-      redirectOutput = false,
-      console = 'integratedTerminal',
-    })
+  table.insert(require('dap').configurations.python, {
+    -- The first three options are required by nvim-dap
+    type = 'python',
+    request = 'launch',
+    name = 'Python3: Launch file',
+    program = '${file}',
+    pythonPath = require('venv-selector').venv(),
+    redirectOutput = false,
+    console = 'integratedTerminal',
+  })
   end,
 }
