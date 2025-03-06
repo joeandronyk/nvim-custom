f1a9aaae812f08e5c5a4b4d68ca3b6293b07c792
Andronyk, Joe <jandronyk@ea.com>
2025-03-06 09:17:51 -0800

    fix(conform): added toggle for formatting on save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -22,12 +22,29 @@ return {
         lua = { 'stylua' },
         python = { 'ruff_fix', 'ruff_format' },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 4000,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
     }
+
+    vim.api.nvim_create_user_command('FormatToggle', function()
+      -- Check the current state and toggle it
+      if vim.g.disable_autoformat or vim.b.disable_autoformat then
+        vim.g.disable_autoformat = false
+        vim.b.disable_autoformat = false
+        print 'Autoformat-on-save enabled'
+      else
+        vim.g.disable_autoformat = true
+        vim.b.disable_autoformat = true
+        print 'Autoformat-on-save disabled'
+      end
+    end, {
+      desc = 'Toggle autoformat-on-save',
+    })
 
     vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
       conform.format {
@@ -36,5 +53,7 @@ return {
         timeout_ms = 1000,
       }
     end, { desc = 'Format file or range (in visual mode)' })
+
+    vim.keymap.set({ 'n' }, '<leader>xf', '<CMD>FormatToggle<CR>', { desc = 'Toggle Format on Save' })
   end,
 }
