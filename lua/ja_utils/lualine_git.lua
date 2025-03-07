54ee64e7a3e1f7cce9683c238bd3ece9dfb02e24
Joe Andronyk <joeandronyk@gmail.com>
2025-03-06 21:18:34 -0800

    simplified the code, needs more testing

--- lua/ja_utils/lualine_git.lua
+++ lua/ja_utils/lualine_git.lua
@@ -68,59 +68,12 @@ end
 ---@param dir_path string|nil
 ---@return string|nil
 function M.find_git_dir(dir_path)
-  local git_dir = vim.env.GIT_DIR
-  -- local git_dir = dir_path or Snacks.git.get_root(cwd)
-  if git_dir then
-    update_current_git_dir(git_dir)
-    return git_dir
-  end
-
   local cwd = vim.fn.getcwd()
-  local file_dir = Snacks.git.get_root(cwd) or cwd
-
-  -- extract correct file dir from terminals
-  if file_dir and file_dir:match 'term://.*' then
-    file_dir = vim.fn.expand(file_dir:gsub('term://(.+)//.+', '%1'))
-  end
-
-  local root_dir = file_dir
-  -- Search upward for .git file or folder
-  while root_dir do
-    if git_dir_cache[root_dir] then
-      git_dir = git_dir_cache[root_dir]
-      break
-    end
-    local git_path = root_dir .. sep .. '.git'
-    local git_file_stat = vim.loop.fs_stat(git_path)
-    if git_file_stat then
-      if git_file_stat.type == 'directory' then
-        git_dir = git_path
-      elseif git_file_stat.type == 'file' then
-        -- separate git-dir or submodule is used
-        local file = io.open(git_path)
-        if file then
-          git_dir = file:read()
-          git_dir = git_dir and git_dir:match 'gitdir: (.+)$'
-          file:close()
-        end
-        -- submodule / relative file path
-        if git_dir and git_dir:sub(1, 1) ~= sep and not git_dir:match '^%a:.*$' then
-          git_dir = git_path:match '(.*).git' .. git_dir
-        end
-      end
-      if git_dir then
-        local head_file_stat = vim.loop.fs_stat(git_dir .. sep .. 'HEAD')
-        if head_file_stat and head_file_stat.type == 'file' then
-          break
-        else
-          git_dir = nil
-        end
-      end
-    end
-    root_dir = root_dir:match('(.*)' .. sep .. '.-')
-  end
+  local file_dir = Snacks.git.get_root() or cwd
+  local git_dir = file_dir .. sep .. '.git'
 
   git_dir_cache[file_dir] = git_dir
+
   if dir_path == nil then
     update_current_git_dir(git_dir)
   end
