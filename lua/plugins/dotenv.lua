843a24c24011038754c5c99c024819a94a11fe98
Joe Andronyk <joeandronyk@gmail.com>
2025-04-19 12:00:33 -0700

    Turning verbose to off to hide the .venv file not found.

--- lua/plugins/dotenv.lua
+++ lua/plugins/dotenv.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require('dotenv').setup {
       enable_on_load = true, -- will load your .env file upon loading a buffer
-      verbose = true, -- show error notification if .env file is not found and if .env is loaded
+      verbose = false, -- show error notification if .env file is not found and if .env is loaded
       file_name = '.env', -- will override the default file name '.env'
     }
   end,
