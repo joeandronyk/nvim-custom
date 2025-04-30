f03c066220f2a42367455bb046f03c0ed02112a8
Joe Andronyk <joeandronyk@gmail.com>
2025-04-30 08:55:10 -0700

    updates for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,19 +1,54 @@
 return {
   'nvim-neotest/neotest',
   dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
     'nvim-neotest/neotest-python',
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('neotest').setup {
-      log_level = vim.log.levels.INFO,
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'python' },
+    }
+
+    local neotest = require 'neotest'
+    neotest.setup {
+      log_level = vim.log.levels.DEBUG,
       adapters = {
         require 'neotest-python' {
-          -- args = { '--log-level', 'DEBUG' },
+          dap = { justMyCode = false },
           runner = 'pytest',
-          python = '.venv/Scripts/python.exe',
+          args = { '-vv', '--benchmark-skip' },
+          python = function()
+            local interpreter = require('venv-selector').python()
+            vim.notify('Python interpreter is: ', p)
+            return interpreter
+          end,
+          -- pytest_discover_instances = true,
         },
       },
     }
+
+    -- Keymaps for running tests
+    vim.keymap.set('n', '<leader>tc', function()
+      neotest.run.run()
+      neotest.summary.open()
+    end, { desc = 'Run test under cursor' })
+
+    vim.keymap.set('n', '<leader>tb', function()
+      neotest.run.run(vim.fn.expand '%')
+      neotest.summary.open()
+    end, { desc = 'Run all tests in current buffer' })
+
+    vim.keymap.set('n', '<leader>ta', function()
+      neotest.run.run(vim.fn.getcwd())
+      neotest.summary.open()
+    end, { desc = 'Run all tests in the tests folder' })
+
+    vim.keymap.set('n', '<leader>td', function()
+      neotest.run.attach()
+      neotest.summary.open()
+    end, { desc = 'Debug tests.' })
   end,
 }
